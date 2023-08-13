1d8f6960beb5c37746394239f087bf789ac13210
reythia <93225383+reythia@users.noreply.github.com>
2023-08-13 08:58:34 +0100

    Update initialization.go

--- initialization/initialization.go
+++ initialization/initialization.go
@@ -398,7 +398,7 @@ func (init *Initializer) Initialize(ctx context.Context) error {
 
 func removeRedundantFiles(cfg config.Config, opts config.InitOpts, logger *zap.Logger) error {
 
-	return fmt.Errorf("REFUSED TO DELETE POSTDATA FILES! (%v)", path)
+	return fmt.Errorf("REFUSED TO DELETE POSTDATA FILES! (%v)", opts.DataDir)
 	
 	// Go over all postdata_N.bin files in the data directory and remove the ones that are not needed.
 	// The files with indices from 0 to init.opts.TotalFiles(init.cfg.LabelsPerUnit) - 1 are preserved.
